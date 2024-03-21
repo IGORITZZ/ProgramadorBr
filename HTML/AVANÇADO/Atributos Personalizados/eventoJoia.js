7b4df0ce23f82b16259593d0f4557dfcf9f53193
Igor Theodoro <IGORITZZ@users.noreply.github.com>
2024-03-21 12:13:16 -0300

    att

--- HTML/AVANÇADO/Atributos Personalizados/eventoJoia.js
+++ HTML/AVANÇADO/Atributos Personalizados/eventoJoia.js
@@ -7,3 +7,16 @@ img.addEventListener('click', function (){
 
     console.log(ultimaimagem);
 })
+
+
+// esse código é usando para quando criamos um elemento html sem colocar o evento DOM no html, etnçao criamos no JavaScript
+// os campos comentados vão simular como ficaria o código dessa outra forma 
+/* 
+function trocarImagem(){
+    let img = document.getElementById('dedo')
+    img.src = 'imagens/joia.png'
+
+    let ultimaimagem = img.getAttribute('src')
+    console.log(ultimaimagem);
+}
+*/
