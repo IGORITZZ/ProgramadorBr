5c96c315eaae88418e2752590c32214769f2d27f
Igor Theodoro <IGORITZZ@users.noreply.github.com>
2024-04-25 17:50:05 -0300

    Update scrip.js

--- HTML/2-AVANÇADO/5-Animação HTML/ex02 - CANVAS (Intro + Linhas)/scrip.js
+++ HTML/2-AVANÇADO/5-Animação HTML/ex02 - CANVAS (Intro + Linhas)/scrip.js
@@ -4,6 +4,11 @@ let context = tela.getContext("2d")
 context.moveTo(0, 0)
 context.lineTo(250, 250)
 context.lineTo(500, 0)
+// lineTo(X, Y) cria uma linha em tela, com base nas posições dos eixos X e Y
 context.strokeStyle = "#FF0000"
+// conseguimos adicionar cor a nossa linha, porém o valor tem que ser entre aspas
 context.lineWidth = 1
+// coseguimos alterar a espeçura da linha
 context.stroke()
+// stroke faz com que a nossa linha seja apresentada na tela
+// sempre tem que ser ao final, pois pode dar erro ou até mesmo não mostrar nada em tela
