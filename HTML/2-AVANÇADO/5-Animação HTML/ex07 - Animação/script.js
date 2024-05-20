60b24396fe1cdf49f42a6918c7acf4d72cac6cae
Igor Theodoro <IGORITZZ@users.noreply.github.com>
2024-05-20 11:51:45 -0300

    att aula animação
    animando um circulo utilizando Js

--- HTML/2-AVANÇADO/5-Animação HTML/ex07 - Animação/script.js
+++ HTML/2-AVANÇADO/5-Animação HTML/ex07 - Animação/script.js
@@ -1,3 +1,39 @@
 let tela = document.getElementById('tela')
 let ctx = tela.getContext('2d')
 
+
+// tranformamos todas as informações que eram variáveis em um objeto
+let circle = {
+    x: 250,
+    y: 250,
+    raio: 100,
+    inicio: 0,
+    fim: 0
+    // uma ultima informação seria a rotação da animação que seria true ou false que mudariam o sentido da rotação do nosso objeto
+}
+
+function drawCircle(c) {
+    ctx.beginPath()
+    ctx.rect(0,0, 500,500)
+    ctx.fillStyle = 'beige'
+    ctx.fill()
+
+    ctx.beginPath()
+    ctx.strokeStyle = 'red'
+    ctx.fillStyle = 'blue'
+    ctx.arc(c.x, c.y, c.raio, c.inicio, c.fim)
+    ctx.fill()
+    ctx.stroke()
+
+}
+
+setInterval(() => {
+    if(circle.fim < 2 * Math.PI){
+        circle.fim += 0.3
+        circle.x += 3
+    }
+
+    drawCircle(circle)
+}, 150);
+
+
