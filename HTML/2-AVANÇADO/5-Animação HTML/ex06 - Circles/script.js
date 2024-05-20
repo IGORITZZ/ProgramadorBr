66cb68d87920ee66c1c582b1b7d36a010ed70889
Igor Theodoro <IGORITZZ@users.noreply.github.com>
2024-05-20 10:54:32 -0300

    att aulas e correções

--- HTML/2-AVANÇADO/5-Animação HTML/ex06 - Circles/script.js
+++ HTML/2-AVANÇADO/5-Animação HTML/ex06 - Circles/script.js
@@ -1,3 +1,19 @@
 let tela = document.getElementById('tela')
 let ctx = tela.getContext('2d')
 
+let x = 250
+// distancia lateral
+let y = 250
+// distancia da parte de cima
+let raio = 100
+// raio do circulo
+let inicio = 0
+let fim = 2 * Math.PI
+// aqui é a pegadinha, sempre a unidade de medida para completar a vonta do circulo é em graus, e para isso é medido em PI, e sendo assim 2 * Math.PI nos da o resultado de uma volta completa
+
+ctx.beginPath()
+ctx.strokeStyle = 'red'
+ctx.fillStyle = 'blue'
+ctx.arc(x, y, raio, inicio, fim)
+ctx.fill()
+ctx.stroke()
