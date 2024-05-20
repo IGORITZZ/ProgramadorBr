ca0ce480f7cce686911efc1ed6e2536b263b519c
Igor Theodoro <IGORITZZ@users.noreply.github.com>
2024-05-20 12:19:39 -0300

    att
    explicações sobre o exercício praticado

--- HTML/2-AVANÇADO/5-Animação HTML/ex07 - Animação/script.js
+++ HTML/2-AVANÇADO/5-Animação HTML/ex07 - Animação/script.js
@@ -12,12 +12,14 @@ let circle = {
     // uma ultima informação seria a rotação da animação que seria true ou false que mudariam o sentido da rotação do nosso objeto
 }
 
+//função para validar os valores de circle
 function drawCircle(c) {
+    //o retangulo para de ser criado em css e começa a ser criado aqui para não bugar o resultado
     ctx.beginPath()
     ctx.rect(0,0, 500,500)
     ctx.fillStyle = 'beige'
     ctx.fill()
-
+    // novamente criamos o circulo
     ctx.beginPath()
     ctx.strokeStyle = 'red'
     ctx.fillStyle = 'blue'
@@ -26,14 +28,20 @@ function drawCircle(c) {
     ctx.stroke()
 
 }
-
+// setInterval define um intervalo de ação entre a condição insposta dentro dela 
 setInterval(() => {
     if(circle.fim < 2 * Math.PI){
+        //se a posição final do circulo for menor que o fechamento dele (valor definido por 2 * Math.PI)
         circle.fim += 0.3
+        // a posição final ira aumentar 0.3, ou seja, fará com que o circulo seja desenhado de maneira devagar
         circle.x += 3
+        // a posição x ira movimentar o ciruculo até que o formato esteja completo
     }
 
     drawCircle(circle)
+    // terminando a condição eu chamo a funcção drawCircle que leva o argumento circle, diferente do definido na funcção em sim. 
+    // o argumento muda aqui para que a função possa ser simplificada somente com "c"
+    // e por ultimo a velocidade do intervalo é de 150ms, definidos no ao final de tudo 
 }, 150);
 
 
